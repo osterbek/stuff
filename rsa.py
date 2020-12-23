6239ec2ddb62f35f37a6f8c7b69995bf138a86b0
osterbek <75850533+osterbek@users.noreply.github.com>
2020-12-23 18:14:54 +0100

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -1,3 +1,5 @@
+import math
+
 def relatively_prime(a, b):
     answer = True
     if a == b:
@@ -9,55 +11,57 @@ def relatively_prime(a, b):
     return answer
 
 
+def prime_factors(number):
+    finding = []
+    for i in range(2, number // 2 + 1):
+        while number % i == 0:
+            finding.append(i)
+            number = number // i
+    return finding
+
+
 if __name__ == '__main__':
+    # take two large prime numbers  p and q
+    p = 13
+    print('first prime p = ', p)
+    q = 53
+    print('second prime q = ', q)
 
-    # Man nehme zwei große Primzahlen p und q
-    p = 5
-    q = 17
-
-    # Man bilde deren Produkt n=p*q, welches Modul genannt wird
+    # form the product n = p * q, called module
     n = p * q
+    print('module n = p * q = ', n)
 
-    # Man wähle eine Zahl e die kleiner als n und teilerfremd (relativ prim) zu p-1 und q-1 ist
-    e = 1
+    # select a number e which is smaller than n and coprime resp. relative prim to p-1 and q-1
+    e = n
     while True:
-        e += 1
+        e -= 1
         if relatively_prime(e, p-1) and relatively_prime(e, p-1):
                 break
+    print('public exponent e = ', e, ' ... is relative prim to p-1 and q-1')
 
-    # Finde eine Zahl d, so dass (e*d)-1 durch (p-1)*(q-1) ohne Rest teilbar ist
+    # select a number d, so that (e*d)-1 is divisible by (p-1)*(q-1) without remainder
     d = 0
     while True:
         d += 1
         if ((e * d) - 1) % ((p - 1)*(q - 1)) == 0:
             break
+    print('private exponent d = ', d, '... corresponds to constraint (e*d)-1 is divisible by (p-1)*(q-1) without remainder')
 
-    # Der öffentliche Schlüssel ist das Paar (n,e),
+    # public key is the pair (n,e),
     public_key = [n, e]
-
-    # der private Schlüssel ist das Paar (n,d).
-    private_key = [n, d]
-
-    # Die Verschlüsselungsfunktion lautet f(m) = c = (m hoch e) mod n
-    # wobei c der Chiffretext und m die Nachricht ist.
-    m = 10
-    cipher = (m ** e) % n
-
-    # Die Entschlüsslungsfunktion lautet: g(c) = (c hoch d) mod n
-    decipher = (cipher ** d) % n
-
-
-
-    print('first prime p = ', p)
-    print('second prime q = ', q)
-    print('module n = ', n)
-    print('public exponent e = ', e)
-    print('private exponent d = ', d)
     print('public key [n, e] = ', public_key)
+
+    # private key is the pair (n,d).
+    private_key = [n, d]
     print('private key [n, d] = ', private_key)
+
+    # The encryption function is f(m) = c = (m powered e) mod n
+    # c is the ciphertext and m is the message.
+    m = 123
     print('message m =', m)
-    print('cipher =', cipher)
-    print('decipher =', decipher)
+    cipher = (m ** e) % n
+    print('cipher = (m ** e) % n =', cipher)
 
-
-
+    # the decipherment function is g(c) = (c powered d) mod n
+    decipher = (cipher ** d) % n
+    print('decipher = (cipher ** d) % n =', decipher)
